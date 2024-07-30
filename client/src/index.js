f9a43ce0d64a33fac6b8a86fc35a16b6697e286d
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-07-30 19:32:31 +0530

    routes added

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider, createBrowserRouter} from "react-router-dom"
-import Home from './views/Home/Home';
-import Login from './views/Login/Login';
-import Signup from './views/Signup/Signup';
+import Home from './views/Home/Home.js';
+import Login from './views/Login/Login.js';
+import Signup from './views/Signup/Signup.js';
 import "./global.css"
-import AddTransaction from './views/AddTransaction/AddTransaction';
+import AddTransaction from './views/AddTransaction/AddTransaction.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
